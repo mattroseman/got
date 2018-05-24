31eae18aea225c8a4388690aedc9ba3d67d7c490
Matthew Roseman <mroseman95@gmail.com>
2018-05-24 16:34:12 -0400

    added PersistentPreRun function to root command to check if directory is got repository, overide it in init command
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,23 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "got",
 	Short: "got is a version control application",
 	Long:  "a implementation of git in golang, for learning purposes",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// check if .got directory doesn't exist
+		if _, err := os.Stat(".got/"); os.IsNotExist(err) {
+			fmt.Println("You are not currently in a got repository")
+			os.Exit(1)
+		}
+	},
 }
 
 // Execute starts up cobra, and runs the rootCmd
