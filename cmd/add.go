26b158f8d42b09c0a047a2b916284a22633ffaa1
Matthew Roseman <mroseman95@gmail.com>
2018-05-24 14:55:37 -0400

    added argument for add command that is a path to a file
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- cmd/add.go
+++ cmd/add.go
@@ -1,15 +1,20 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 // TODO check that the current directory is a .got repository
 
 var addCmd = &cobra.Command{
-	Use:   "add",
+	Use:   "add [path to file to add]",
 	Short: "Add a file to be tracked by .got",
 	Long:  "Compresses a file and adds it to .got/objects to be tracked",
-	// TODO give this command arguments
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		add(args[0])
 	},
 }
 
@@ -20,4 +25,5 @@ func init() {
 // add takes a path to a file, and compresses that file adding it to
 // .got/objects
 func add(filePath string) {
+	fmt.Printf("TODO add %s to .got directory\n", filePath)
 }
