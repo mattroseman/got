c1fbb5824de79f932306c6fae06b2305ea30caad
Matthew Roseman <mroseman95@gmail.com>
2018-05-23 20:48:48 -0400

    init command creates .got and objects directories
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- cmd/init.go
+++ cmd/init.go
@@ -1,17 +1,40 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a new got repository",
 	Long:  "Create a new got repository for managing version control of current directory",
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO create .got directory
-		// TODO create .got/objects directory
+		if err := initGot(); err != nil {
+			fmt.Println(err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+}
+
+// initGot initializes a new got repository.
+// returns error if .got directory already exists
+func initGot() error {
+	// check if .got directory already exists
+	if _, err := os.Stat(".got/"); err == nil {
+		return errors.New("This directory is already a got repository")
+	}
+
+	// creat .got directory and objects subdirectory
+	if err := os.MkdirAll(".got/objects", 0755); err != nil {
+		return err
+	}
+
+	return nil
 }
