735affd5ae39bea07fdc301e6897fbbc06255633
Matthew Roseman <mroseman95@gmail.com>
2018-05-24 19:05:03 -0400

    root.go now checks if command is being called within subdirectory of got repository
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -7,13 +7,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var gotRootDir string
+
 var rootCmd = &cobra.Command{
 	Use:   "got",
 	Short: "got is a version control application",
 	Long:  "a implementation of git in golang, for learning purposes",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		// check if .got directory doesn't exist
-		if _, err := os.Stat(".got/"); os.IsNotExist(err) {
+		// check to see if current directory is in a got repository
+		var ok bool // declare using var because := can't be used with global gotRootDir
+		gotRootDir, ok = getGotRootDir()
+		if !ok {
 			fmt.Println("You are not currently in a got repository")
 			os.Exit(1)
 		}
